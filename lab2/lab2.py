6205da67689cf64f5afdf1eb51f326592847c0ac
WojtekG98 <48157827+WojtekG98@users.noreply.github.com>
2020-03-24 11:57:27 +0100

    Add files via upload
    Algorytm Schrage ale cos nie dziala dobrze

---
+++ lab2/lab2.py
@@ -0,0 +1,104 @@
+import os
+
+
+def pobierz_dane(plik):
+    """
+    Funkcja zwraca tuplę tupli zawierających dane pobrane z pliku csv
+    do zapisania w tabeli.
+    """
+    dane = []  # deklarujemy pustą listę
+    if os.path.isfile(plik):  # sprawdzamy czy plik istnieje na dysku
+        with open(plik, "r") as zawartosc:  # otwieramy plik do odczytu
+            i = 0
+            for linia in zawartosc:
+                linia = linia.replace("\n", "")  # usuwamy znaki końca linii
+                linia = linia.replace("\r", "")  # usuwamy znaki końca linii
+                # linia = linia.decode("utf-8")  # odczytujemy znaki jako utf-8
+                # dodajemy elementy do tupli a tuplę do listy
+                x = map(int, linia.split(" "))
+                x = list(x)
+                x.append(i)
+                dane.append(tuple(x))
+                i = i + 1
+    else:
+        print("Plik z danymi", plikcsv, "nie istnieje!")
+    return tuple(dane)  # przekształcamy listę na tuplę i zwracamy ją
+
+
+def kolejnosc(lista):
+    y = []
+    for i in range(len(lista)):
+        y.append(lista[i][3])
+    return y
+
+
+def calculate(tablica, n):
+    #   S(0)=r_pi(0)
+    S = [tablica[0][0]]
+    #   C(0)=S_0 + p_pi(0)
+    C = [S[0] + tablica[0][1]]
+    #   C_max=C(0) + q_pi(0)
+    Cmax = C[0] + tablica[0][2]
+    for i in range(1, n):
+        # S(i)=max(r_pi(i),c_i-1)
+        S.append(max(tablica[i][0], C[i - 1]))
+        # C(i)=S(i)+p_pi(i)
+        C.append(S[i] + tablica[i][1])
+        # C_max=max(C_max,C(i)+q_pi(i))
+        Cmax = max(Cmax, C[i] + tablica[i][2])
+    return Cmax
+
+
+def Schrage(tablica, n):
+    k = 1
+    # G - zbiór zadań gotowych do realizacji
+    G = []
+    # N - zbiór zadań nieuszeregowanych
+    N = list(tablica)
+    # t - zmienna pomocnicza symbolizująca chwilę czasową
+    # t = min(r_j) z N
+    t = min(N, key=lambda x: x[0])[0]
+    wynik = []
+    while len(G) != 0 or len(N) != 0:
+        while len(N) != 0 and t <= min(N, key=lambda x: x[0])[0]:
+            j = min(N, key=lambda x: x[0])
+            G.append(j)
+            N.remove(j)
+        if len(G) != 0:
+            j = max(G,key=lambda x: x[2])
+            G.remove(j)
+            wynik.append(j)
+            t = t + j[1]
+            k = k + 1
+        else:
+            t = min(N, key=lambda x: x[0])[0]
+    return wynik
+
+
+def lab1(pliki):
+    for j in range(0, 6):
+        dane = pobierz_dane(pliki[j])
+        n = dane[0][0]
+        r = dane[0][1]
+        dane = list(dane[1:n + 1])
+        wynik = calculate(dane, n)
+        print("Nazwa pliku: ", pliki[j])
+        print("Wynik nieposortowany:", wynik)
+        print(kolejnosc(dane))
+        dane.sort(key=lambda x: x[0])
+        wynik = calculate(dane, n)
+        print("Wynik posortowany:", wynik)
+        print(kolejnosc(dane))
+
+
+pliki = ["data10.txt", "data20.txt", "data50.txt", "data100.txt", "data200.txt", "data500.txt"]
+dane = pobierz_dane(pliki[0])
+n = dane[0][0]
+r = dane[0][1]
+dane = list(dane[1:n + 1])
+dane=tuple(dane)
+print(kolejnosc(dane))
+wynikalg=Schrage(dane, n)
+
+print(kolejnosc(wynikalg))
+print(calculate(wynikalg,n))
