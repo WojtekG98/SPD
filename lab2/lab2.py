ecc6b5520fa66fa11ddf164636e6513d5802fc80
WojtekG98 <48157827+WojtekG98@users.noreply.github.com>
2020-03-30 19:10:02 +0200

    Add files via upload
    Dodano Carlier działający

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,8 +1,8 @@
 import os
 import time
 import heapq
-import queue
-
+import math
+import copy
 def pobierz_dane(plik):
     """
     Funkcja zwraca tuplę tupli zawierających dane pobrane z pliku csv
@@ -18,11 +18,11 @@ def pobierz_dane(plik):
                 x = map(int, linia.split(" "))   # dodajemy elementy do tupli a tuplę do listy
                 x = list(x)                      # tutaj takie wygibasy żeby dodać czwarta liczbę
                 x.append(i)                      # która jest kolejnością zadań
-                dane.append(tuple(x))            # pewnie da się ładniej ale jestem nowy w Python
+                dane.append(list(x))            # pewnie da się ładniej ale jestem nowy w Python
                 i = i + 1                        # i++ zadania są numerowane od 1 do n
     else:
         print("Plik z danymi", plik, "nie istnieje!")
-    return tuple(dane)  # przekształcamy listę na tuplę i zwracamy ją
+    return list(dane)  # przekształcamy listę na tuplę i zwracamy ją
 
 
 def kolejnosc(lista):
@@ -165,19 +165,79 @@ def SchragePmtn_queue(tablica, n):
             t = N[0][0]                           # t = min r_j z N (minimum z N posortowane po r)
     return Cmax
 
-def Carlier(tablica, n):
-    UB=1000000
-    wynik=list(tablica)
-    U = Schrage(tablica, n)
-    if U < UB:
-        UB=calculate(U,n)
-        wynik_optymalny = U
-    #a=
-    #b=
-    c=0
-    if len(c)==0:
-        return wynik_optymalny
+def Carlier(tablica, n):                            # funkcja wywolujaca funkcje bo inaczej
+                                                    # nie dzialalo przekazywanie UB przez referencje
+    UB = [math.inf]                                 # UB = inf
+    do_Carlier(tablica, UB)                         # wykonaj Carliera
+    return UB[0]                                    # zwroc wynik
 
+def do_Carlier(tablica, UB):
+    Pi = Schrage(tablica, len(tablica))
+    U = calculate(Pi,len(Pi))                       # U <- Schrage(J)
+    if U < UB[0]:                                   # jeżeli U   UB, a UB[0] bo chcemy udawana referencje mieć
+        UB[0]=U                                     # UB = U
+    b, c = znajdz_b_c(Pi)                           # a jest potrzebne tylko do szukania c
+    if c is None:                                   # jeżeli c puste to return
+        return
+    K = znajdz_k(Pi, c, b)                          # K = Pi[c,b]
+    r = min(K, key=lambda x: x[0])[0]               # r = min r z K
+    q = min(K, key=lambda x: x[2])[2]               # q = min q z K
+    p = 0
+    for item in K:
+        p = p + item[1]                             # p = sum p_i, i z K
+    r_tmp = c[0]
+    c[0]=max(r_tmp, r + p)                          # r_pi = max(r_pi, r+p)
+    LB = SchragePmtn(Pi,len(Pi))                    # LB = SchragePmtn(Pi)
+    if LB < UB[0]:                                  # jeżeli U   UB, a UB[0] bo chcemy udawana referencje mieć
+        do_Carlier(Pi, UB)                          # rekurencja
+    c[0]=r_tmp                                      # restore r_pi(c)
+    q_tmp = c[2]
+    c[2]= max(q_tmp, q + p)                          # q_pi = max (q_pi, q + p)
+    LB = SchragePmtn(Pi,len(Pi))
+    if LB < UB[0]:                                  # jeżeli U   UB, a UB[0] bo chcemy udawana referencje mieć
+        do_Carlier(Pi,UB)                           # rekurencja
+    c[2] = q_tmp                                    # restore q_pi(c)
+
+def znajdz_b_c(Pi):
+        t = 0
+        Cmax = calculate(Pi,len(Pi))
+        b = None
+        for zadanie in Pi:
+            t = max(t, zadanie[0]) + zadanie[1]
+            if t + zadanie[2] == Cmax:
+                b = zadanie
+        a = None
+        p = 0
+        for zadanie in Pi:
+            p = p + zadanie[1]
+            if zadanie == b:
+                break
+
+        for zadanie in Pi:
+            if Cmax == zadanie[0] + p + b[2]:
+                a = zadanie
+                break
+            p = p - zadanie[1]
+            if zadanie == b:
+                break
+
+        c = None
+        for zadanie_index in range(Pi.index(a), Pi.index(b)):
+            if b[2] > Pi[zadanie_index][2]:
+                c = Pi[zadanie_index]
+        return b, c
+
+def znajdz_k(Pi, c, b):
+        k = []
+        czy_dodac = False
+        for zadanie in Pi:
+            if czy_dodac:
+                k.append(zadanie)
+            if c == zadanie:
+                czy_dodac = True
+            if zadanie == b:
+                break
+        return k
 
 def lab1(pliki):
     for j in range(0, 6):
@@ -222,6 +282,16 @@ def lab2(pliki):
         end=time.time_ns()
         print("Cmax SchragePmtn_queue=", Cmax,", w czasie:",(end-start)/1000000," ms")
 
+def lab3(pliki):
+    for i in range(0, len(pliki)):
+        dane = pobierz_dane(pliki[i])
+        n = dane[0][0]
+        r = dane[0][1]
+        dane = (dane[1:n + 1])
+        odp = Carlier(dane, n)
+        print(odp)
 
 pliki = ["data10.txt", "data20.txt", "data50.txt", "data100.txt", "data200.txt", "data500.txt"]
-lab2(pliki)
+#lab1(pliki)
+#lab2(pliki)
+lab3(pliki)
